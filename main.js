80a99822daf42ee1e1add346496f8b80f5edc07a
skystar <usezmap@gmail.com>
2018-11-26 02:10:37 +0900

    style

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-let browser;
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+let browser
+
+const INITIAL_CHANNEL_NAME = 'uriirc'
 
 function log(message) {
-    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-    console.log(`LOG [${date}] ${message}`);
+    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    console.log(`LOG [${date}] ${message}`)
 }
 
 const { username, password } =
-    JSON.parse(fs.readFileSync('./secret.json').toString('utf8'));
+    JSON.parse(fs.readFileSync('./secret.json').toString('utf8'))
 
 process.on('SIGINT', async () => {
-    log('Received SIGINT. Good bye!');
-    await browser.close();
-    process.exit(0);
-});
+    log('Received SIGINT. Good bye!')
+    await browser.close()
+    process.exit(0)
+})
 
 // function for login to irccloud.com
 async function login(username, password) {
-    const page = await browser.newPage();
+    const page = await browser.newPage()
 
-    const URL = 'https://www.irccloud.com/';
-    log('Navigating to irccloud...');
-    await page.goto(URL, { timeout: 0 });
+    const URL = 'https://www.irccloud.com/'
+    log('Navigating to irccloud...')
+    await page.goto(URL, { timeout: 0 })
 
-    log('Trying to login...');
-    await page.waitForSelector('p.form>input[name=email]');
-    await page.waitForSelector('p.form>input[name=password]');
-    await page.waitForSelector('p.form>button[type=submit]');
+    log('Trying to login...')
+    await page.waitForSelector('p.form>input[name=email]')
+    await page.waitForSelector('p.form>input[name=password]')
+    await page.waitForSelector('p.form>button[type=submit]')
 
-    await page.type('p.form>input[name=email]', username);
-    await page.type('p.form>input[name=password]', password);
+    await page.type('p.form>input[name=email]', username)
+    await page.type('p.form>input[name=password]', password)
 
-    await page.click('p.form>button[type=submit]');
+    await page.click('p.form>button[type=submit]')
 
-    await page.waitForNavigation();
-    log('Login done!');
+    await page.waitForNavigation()
+    log('Login done!')
 
-    return page;
+    const CHANNEL_URL = `https://www.irccloud.com/irc/${INITIAL_CHANNEL_NAME}`
+    await page.goto(CHANNEL_URL, { timeout: 0 })
+
+    return page
 }
 
 // main
@@ -45,7 +50,7 @@ async function login(username, password) {
     opts = {
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true,
-    };
-    browser = await puppeteer.launch(opts);
-    await login(username, password);
-})();
+    }
+    browser = await puppeteer.launch(opts)
+    await login(username, password)
+})()
